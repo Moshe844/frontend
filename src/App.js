42ee0131c36607737c8a83d04614c4d5cd7e4724
MOshea <arye6700@gmail.com>
2022-09-09 12:09:49 -0400

    update

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
       })
       .then(response => response.json())
       .then(response => {
-        if (response) {
+        if (response.id) {
           fetch('https://ancient-sea-46547.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
