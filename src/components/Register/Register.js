3bfaea3cb6869585b4a42d5f17a58a538476257a
MOshea <arye6700@gmail.com>
2022-09-15 12:50:59 -0400

    updating

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,95 +1,121 @@
 import React from 'react';
-// import { toast} from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-
-// toast.configure()
 class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       email: '',
       password: '',
-      name: ''
-    }
+      name: '',
+    };
   }
 
-  onNameChange = (event) => {
-    this.setState({name: event.target.value})
-  }
+  showToast = message => {
+    toast.error(message, {
+      position: 'top-right',
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
 
-  onEmailChange = (event) => {
-    this.setState({email: event.target.value})
-  }
+  onNameChange = event => {
+    this.setState({ name: event.target.value });
+  };
 
-  onPasswordChange = (event) => {
-    this.setState({password: event.target.value})
-  }
+  onEmailChange = event => {
+    this.setState({ email: event.target.value });
+  };
 
-  onSubmitSignIn = (event) => {
-    event.preventDefault()
+  onPasswordChange = event => {
+    this.setState({ password: event.target.value });
+  };
+
+  onSubmitSignIn = event => {
+    event.preventDefault();
+    let regex = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/g;
+    const isEmailValid = this.state.email.match(regex);
+    if (!isEmailValid) return this.showToast();
     fetch('https://ancient-sea-46547.herokuapp.com/register', {
       method: 'post',
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         email: this.state.email,
         password: this.state.password,
-        name: this.state.name
-      })
+        name: this.state.name,
+      }),
     })
-      .then(response => response.json())
+      .then(response => {
+        if (!response.ok) throw new Error('email already taken');
+        return response.json();
+      })
       .then(user => {
         if (user.id) {
-          this.props.loadUser(user)
-          this.props.onRouteChange('home');
+          toast.success('registration was successful');
+          setTimeout(() => {
+            this.props.loadUser(user);
+            this.props.onRouteChange('home');
+          }, 2000);
         }
       })
-  }
+      .catch(error => this.showToast(error.message));
+  };
 
   render() {
     return (
-      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 black-80">
-          <form className="measure">
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0">Register</legend>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+      <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
+        <ToastContainer />
+        <main className='pa4 black-80'>
+          <form className='measure'>
+            <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
+              <legend className='f1 fw6 ph0 mh0'>Register</legend>
+              <div className='mt3'>
+                <label className='db fw6 lh-copy f6' htmlFor='name'>
+                  Name
+                </label>
                 <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="text"
-                  name="name"
-                  id="name"
+                  className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                  type='text'
+                  name='name'
+                  id='name'
                   onChange={this.onNameChange}
                 />
               </div>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+              <div className='mt3'>
+                <label className='db fw6 lh-copy f6' htmlFor='email-address'>
+                  Email
+                </label>
                 <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="email"
-                  name="email-address"
-                  id="email-address"
+                  className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                  type='email'
+                  name='email-address'
+                  id='email-address'
                   onChange={this.onEmailChange}
                 />
               </div>
-              <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+              <div className='mv3'>
+                <label className='db fw6 lh-copy f6' htmlFor='password'>
+                  Password
+                </label>
                 <input
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="password"
-                  name="password"
-                  id="password"
+                  className='b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                  type='password'
+                  name='password'
+                  id='password'
                   onChange={this.onPasswordChange}
                 />
               </div>
             </fieldset>
-            <div className="">
+            <div className=''>
               <input
                 onClick={this.onSubmitSignIn}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                type="submit"
-                value="Register"
+                className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib'
+                type='submit'
+                value='Register'
               />
             </div>
           </form>
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,60 +1,77 @@
-import React from 'react';
-// import { toast} from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
-
-// toast.configure()
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 class Signin extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      signInEmail: '',
-      signInPassword: ''
-    }
+      signInEmail: "",
+      signInPassword: "",
+    };
   }
+  showToast = () => {
+    toast.error("invalid username or password", {
+      position: "top-right",
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
+  // notify = (notifying) => {
+  //   toast('invalid username or password')
+  // }
 
   onEmailChange = (event) => {
-    this.setState({signInEmail: event.target.value})
-  }
+    this.setState({ signInEmail: event.target.value });
+  };
 
   onPasswordChange = (event) => {
-    this.setState({signInPassword: event.target.value})
-  }
-
+    this.setState({ signInPassword: event.target.value });
+  };
 
   onSubmitSignIn = (event) => {
-    event.preventDefault()
-    fetch('https://ancient-sea-46547.herokuapp.com/signin', {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
+    event.preventDefault();
+    let regex = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/g;
+    const isEmailValid = this.state.signInEmail.match(regex);
+    if (!isEmailValid) return this.showToast();
+    fetch("https://ancient-sea-46547.herokuapp.com/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         email: this.state.signInEmail,
-        password: this.state.signInPassword
-      })
+        password: this.state.signInPassword,
+      }),
     })
-      .then(response => response.json())
-      .then(user => {
+      .then((response) => {
+        if (!response.ok) throw new Error("invalid");
+        return response.json();
+      })
+      .then((user) => {
         if (user.id) {
-          this.props.loadUser(user)
-          this.props.onRouteChange('home');
+          this.props.loadUser(user);
+          this.props.onRouteChange("home");
         }
-      }).catch(error => {
-        console.log("some error occurred", error);
-});
-
-  }
+      })
+      .catch((error) => this.showToast());
+  };
 
   render() {
     const { onRouteChange } = this.props;
     return (
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <ToastContainer />
         <main className="pa4 black-80">
           <form className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
                 <input
                   className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                   type="email"
@@ -64,7 +81,9 @@ class Signin extends React.Component {
                 />
               </div>
               <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
                 <input
                   className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                   type="password"
@@ -76,14 +95,19 @@ class Signin extends React.Component {
             </fieldset>
             <div className="">
               <input
-                onClick={this.onSubmitSignIn} 
+                onClick={this.onSubmitSignIn}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
                 value="Sign in"
               />
             </div>
             <div className="lh-copy mt3">
-              <p  onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+              <p
+                onClick={() => onRouteChange("register")}
+                className="f6 link dim black db pointer"
+              >
+                Register
+              </p>
             </div>
           </form>
         </main>
